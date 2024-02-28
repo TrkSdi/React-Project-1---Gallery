d24f64ef0bdecc2d8ccd7e3238c69fa9da881804
Tarik SADKHI <tarik.sadkhi@gmail.com>
2024-02-28 11:18:36 +0100

    css final v1

--- src/components/main-pictures/mainpic.jsx
+++ src/components/main-pictures/mainpic.jsx
@@ -5,14 +5,13 @@ import "./mainpic.css"
 export default function MainPicture() {
     return (
     <>
-    <div className="top">
-        <h1>URBAN GALLERY</h1>
-        <img className="main-pic" src={PICTURES[0].image} alt="couloir rose" />
-    </div>
-    <div className="low">
-        <Caroussel/>
-    </div>
-    
+        <div className="top">
+            <h1>URBAN GALLERY</h1>
+            <img className="main-pic" src={PICTURES[0].image} alt="couloir rose" />
+        </div>
+        <div className="low">
+            <Caroussel/>
+        </div>
     </>
         
     )
